f0a2ce844202167af5c084858fc94e91f5f83011
KKorionKK-Mac <cool.orion2015@yandex.ru>
2023-07-04 13:58:36 +0500

    добавил docker-compose

--- internal/database.py
+++ internal/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-conn_string = 'postgresql+psycopg2://postgres:123gr@localhost:4321/social_app'
+conn_string = 'postgresql+psycopg2://postgres:123gr@db:5432/social_app'
 
 engine = create_engine(conn_string)
 
